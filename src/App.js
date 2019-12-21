61ac64e8dd09ccbda42c15baabd7b517a9a8cc85
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2019-12-21 13:23:42 +0530

    add Account component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import TransactionsTable from './components/TransactionsTable';
+import Account from './components/Account';
+
 import data from './mockData';
 
 function App() {
   return (
     <div>
+      <Account
+        accountNum={data[0]['Account No']}
+        amount={data[data.length - 1]['Balance AMT']}
+      ></Account>
       <TransactionsTable transactions={data}></TransactionsTable>
     </div>
   );
---
+++ src/components/Account.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Account({ accountNum, amount }) {
+  return (
+    <div>
+      <div>
+        <i className='fas fa-user-tie fa-10x'></i>
+      </div>
+      <h1>A/C No. : {accountNum}</h1>
+      <h2>Balance:{amount}</h2>
+    </div>
+  );
+}
+
+export default Account;
