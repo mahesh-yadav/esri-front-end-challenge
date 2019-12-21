db48de5e2daf4cd606ae3907715591a6e475a7a9
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2019-12-21 15:40:36 +0530

    add Account component styles

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import styles from '../styles/Account.module.css';
 
 function Account({ accountNum, amount }) {
   return (
-    <div>
-      <div>
+    <div className={styles['account']}>
+      <div className={styles['account-icon']}>
         <i className='fas fa-user-tie fa-10x'></i>
       </div>
-      <h1>A/C No. : {accountNum}</h1>
-      <h2>
-        Balance: <i className='fas fa-rupee-sign'></i> {amount}
-      </h2>
+      <div className={styles['account-info']}>
+        <h1>A/C No. : {accountNum}</h1>
+        <h2>
+          Balance: <i className='fas fa-rupee-sign'></i> {amount}
+        </h2>
+      </div>
     </div>
   );
 }
